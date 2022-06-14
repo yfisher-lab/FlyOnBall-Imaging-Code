d783e185e3fb6c27ffe0e699bba5cdcb790598b8
Yvette <evettita@gmail.com>
2022-06-13 20:16:24 -0700

    fix packet reading from serial port

--- sample_daq_io.m
+++ sample_daq_io.m
@@ -10,6 +10,8 @@ addinput(dq,"Dev1", "ai4","Voltage"); % add piezo position output
 addinput(dq,"Dev1", "ai5","Voltage"); % add res frame start trigger 
 addinput(dq,"Dev1", "ai6","Voltage"); % add Z trigger volume start
 addoutput(dq, "Dev1", "ao0", "Voltage"); % add AO primary channel (output device timing triggers 100 Hz)
+addoutput(dq,"Dev1", "Port0/Line0","Digital")
+addoutput(dq,"Dev1", "Port0/Line1","Digital")
 
 dq.Channels(1).TerminalConfig = 'SingleEnded'; %save info that channel is in single ended on BOB 
 dq.Channels(2).TerminalConfig = 'SingleEnded';
@@ -18,6 +20,7 @@ dq.Channels(4).TerminalConfig = 'SingleEnded';
 dq.Channels(5).TerminalConfig = 'SingleEnded';
 dq.Channels(6).TerminalConfig = 'SingleEnded';
 dq.Channels(7).TerminalConfig = 'SingleEnded';
+
 
 %% Build commandOut for triggering devices 
 % create empty commmand out array
@@ -40,10 +43,14 @@ for i = 1: int16(dq.Rate/TRIGGER_RATE) :length(commandOut)
     commandOut(i: i + up_frames-1) = HIGHVOLTAGE;
 end
 
+dig_out = zeros(fullTime * dq.Rate, 1);
+dig_out(floor(dq.Rate*fullTime/2):end-10) = 1;
 
+dig_out2 = zeros(fullTime * dq.Rate, 1);
+dig_out2(3*floor(dq.Rate*fullTime/4):end-10) = 1;
 
 %% Recording the data in forground
-data = readwrite(dq, commandOut);
+data = readwrite(dq, [commandOut, dig_out, dig_out2]);
 
 % Store ball heading and panel position (x_pos) values in mV
 x_pos = (data.Dev1_ai0); % DAC0 output from controller gives x frame 
